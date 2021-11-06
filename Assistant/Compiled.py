885dc4c99a644363768590912093a6d5ee25c28d
Vivek Sharma <vivshrmav4@gmail.com>
2021-11-06 12:27:39 +0530

    CHrome Links

--- Assistant/Compiled.py
+++ Assistant/Compiled.py
@@ -3,6 +3,7 @@ import smtplib
 import wikipedia
 import speech_recognition as sr
 import pyttsx3 as p
+import webbrowser as wb
 
 engine = p.init()
 
@@ -45,9 +46,9 @@ def greet():
     hr = datetime.datetime.now().hour
     if 4 < hr < 12:
         say("Good Morning Sir, Welcome back!")
-    elif 12 < hr < 18:
+    elif 12 <= hr < 18:
         say("Good Afternoon Sir, Welcome back!")
-    elif 18 < hr <= 22:
+    elif 18 <= hr <= 22:
         say("Good Evening Sir, Welcome back!")
     else:
         say("Good Night Sir, Welcome back!")
@@ -59,7 +60,6 @@ def tc():
     # source: sr.Microphone
     with sr.Microphone() as source:
         print("Listening:")
-        say("Listening")
         audio = r.listen(source)
     try:
         print("Recognizing")
@@ -81,9 +81,10 @@ if __name__ == "__main__":
         elif "date" in qry:
             date()
         elif "wikipedia" in qry:
-            say("Searching")
-            qry = qry.replace("wikipedia", "")
-            result = wikipedia.summary(qry, sentences=3)
+            say("What should I Search?")
+            search = tc().lower()
+            qry = qry.replace("wikipedia", search)
+            result = wikipedia.summary(qry, sentences=2)
             say(result)
         elif "email" in qry:
             try:
@@ -97,5 +98,10 @@ if __name__ == "__main__":
                 say("Unable to send the Email because of:")
                 say(E1)
                 print(E1)
+        elif "chrome" in qry:
+            say("What Should I search?")
+            path = "C:/Program Files/Google/Chrome/Application/chrome.exe %s"
+            search = tc().lower()
+            wb.get(path).open_new_tab(search + ".com")
         elif "offline" in qry:
             quit()
